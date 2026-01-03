4c26e314274853a745295f3081c53cd11722fc2e
symi-daguo <symi-daguo@gmail.com>
2026-01-03 14:35:27 +0800

    refactor: rename apps to Apps for CasaOS AppStore compliance and fix icon URLs

--- packages/frontend/src/hooks/app-info.ts
+++ packages/frontend/src/hooks/app-info.ts
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import packageJson from "../../../../apps/home-assistant-matter-hub/package.json";
+import packageJson from "../../../../Apps/home-assistant-matter-hub/package.json";
 
 export interface AppInfo {
   name: string;
